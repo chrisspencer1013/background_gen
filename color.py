6b59f802cb556c278359b4f7df5b9a7504ad85b7
Chris Spencer <chrisspencer1013@gmail.com>
2019-05-07 15:43:50 -0400

    Update color.py

--- color.py
+++ color.py
@@ -4,6 +4,7 @@
 from random import randint
 from PIL import Image, ImageDraw
 import math
+#TODO: logging... shouldve started with it lul
 
 SCREEN_SIZE = (1920, 1080)
 WHITE = (255, 255, 255)
@@ -23,22 +24,25 @@ def rand_rgb(bright=None):
             color[randint(0,2)] -= 20
         return tuple(color)
 
-def coords_diamond(center_coords):
-    x = center_coords[0]
-    y = center_coords[1]
-    z = randint(1,3)
+
+def coords_diamond(center, z = None):
+    x = center[0]
+    y = center[1]
+    z = randint(1,3) if not z else z
     return [(x+z, y), (x, y+z), (x-z, y), (x, y-z)]
 
-def coords_circle(center_coords):
-    x = center_coords[0]
-    y = center_coords[1]
-    z = randint(1,3)
+def coords_circle(center, z = None):
+    x = center[0]
+    y = center[1]
+    z = randint(1,3) if not z else z
     return [(x-z, y-z), (x+z, y+z)]
 
 class Backgroundifier():
     
     def __init__(self):
         self.CANVAS_SIZE = SCREEN_SIZE
+        self.img = None
+        self.draw = None
 
     def rand_point(self, boarder = False, first_point = None): 
         if boarder:
@@ -61,41 +65,58 @@ class Backgroundifier():
         #print(f"Line coords: {point1}, {point2}")
         return (point1[0], point1[1], point2[0], point2[1])
 
-    def draw_lines(self):
+    def background_lines(self):
         # gen larger and sample down with antialiasing
         self.CANVAS_SIZE = (SCREEN_SIZE[0]*2, SCREEN_SIZE[1]*2)
-        img = Image.new('RGB', self.CANVAS_SIZE, BLACK)
-        draw = ImageDraw.Draw(img)
+        self.img = Image.new('RGB', self.CANVAS_SIZE, BLACK)
+        self.draw = ImageDraw.Draw(self.img)
         for i in range(randint(1,25)):
-            draw.line(self.rand_line(), rand_rgb(True), width=4)
-        img.resize(SCREEN_SIZE, Image.ANTIALIAS)
-        return img
+            self.draw.line(self.rand_line(), rand_rgb(True), width=4)
+        self.img.resize(SCREEN_SIZE, Image.ANTIALIAS)
 
-    def draw_stars(self):
+    def background_stars(self):
         self.CANVAS_SIZE = SCREEN_SIZE
-        img = Image.new('RGB', self.CANVAS_SIZE, BLACK)
-        draw = ImageDraw.Draw(img)
+        self.img = Image.new('RGB', self.CANVAS_SIZE, BLACK)
+        self.draw = ImageDraw.Draw(self.img)
         for i in range(randint(1000,5000)): 
-            draw.point(self.rand_point(), rand_rgb(True))
+            self.draw.point(self.rand_point(), rand_rgb(True))
         for i in range(randint(10,30)): 
             if randint(0,1):
-                draw.polygon(coords_diamond(self.rand_point()), rand_rgb(True))
+                self.draw.polygon(coords_diamond(center=self.rand_point()), rand_rgb(True))
             else:
-                draw.ellipse(coords_circle(self.rand_point()), rand_rgb(True))
-        img.resize(SCREEN_SIZE, Image.ANTIALIAS)
-        return img
+                self.draw.ellipse(coords_circle(center=self.rand_point()), rand_rgb(True))
+        self.img.resize(SCREEN_SIZE, Image.ANTIALIAS)
+    
+    def generate_shape(self):
+        shape = randint(0,3)
+        if shape == 0: #TODO: dicitonary function mapping? #small circle
+            self.draw.ellipse(coords_circle(center=self.rand_point(), z=randint(40,300)), rand_rgb(True))
+        elif shape == 1: #large circle
+            self.draw.ellipse(coords_circle(center=self.rand_point(), z=randint(400,1000)), rand_rgb(True))
+        elif shape == 2: #square (yes, this uses coords_circle, i know i should make it generic TODO)
+            self.draw.rectangle(coords_circle(center=self.rand_point(), z=randint(40,300)), rand_rgb(True))
+        #todo
+        
 
-    def draw(self):
-        background_type = randint(0,1)
-        img = None
+    def background_shapes(self):
+        self.CANVAS_SIZE = (SCREEN_SIZE[0]*2, SCREEN_SIZE[1]*2)
+        self.img = Image.new('RGB', self.CANVAS_SIZE, BLACK)
+        self.draw = ImageDraw.Draw(self.img)
+        for i in range(randint(2,5)):
+            self.generate_shape()
+            
+    def create_background(self):
+        background_type = 2 #randint(0,1)
         if background_type == 0:
-            img = self.draw_lines()
+            self.background_lines()
         elif background_type == 1:
-            img = self.draw_stars()
-        elif background_type > 1:
-            print('nope')
-        img.show()
+            self.background_stars()
+        elif background_type == 2:
+            self.background_shapes()
+        else:
+            print('oof')
+        self.img.show()
 
 if __name__ == '__main__':
-    background = Backgroundifier()
-    background.draw()
+    b = Backgroundifier()
+    b.create_background()
